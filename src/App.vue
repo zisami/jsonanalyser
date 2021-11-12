f4af5b15f8b60b0ad834595b375b2deef61e1e1a
zisami <sascha.zika@gmx.at>
2021-11-12 16:03:56 +0100

    Vue Test Utils Fixes #16

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,39 @@
 <template>
   <div id="app">
-    <div
-      id="nav"
-      class="hidden"
-    >
-      <router-link to="/">
-        Home
-      </router-link> |
-      <router-link to="/about">
-        About
-      </router-link>
+    <div id="nav" class="hidden">
+      <router-link to="/" name="home"> Home </router-link> |
+      <router-link to="/about" name="about"> About </router-link>
     </div>
     <router-view />
   </div>
 </template>
 <script>
-
-
 export default {
-    data() {
-        return {};
-    },
-    created: function () {},
+  data() {
+    return {};
+  },
+  created: function () {},
 };
 </script>
 <style lang="postcss">
 body {
-    @apply bg-gray-800;
-    position: relative;
+  @apply bg-gray-800;
+  position: relative;
 }
 #app {
-    @apply text-gray-300;
+  @apply text-gray-300;
 }
 
 #nav {
-    padding: 30px;
+  padding: 30px;
 
-    a {
-        font-weight: bold;
-        color: #2c3e50;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-        &.router-link-exact-active {
-            color: #42b983;
-        }
+    &.router-link-exact-active {
+      color: #42b983;
     }
+  }
 }
 </style>
